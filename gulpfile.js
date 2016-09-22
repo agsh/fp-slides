b80eef2eead9daf27552cea8ec4ba22474e51a25
Andrew D.Laptev <a.d.laptev@gmail.com>
2016-09-22 21:38:52 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp')
+	, pug = require('gulp-pug')
+	, reveal = require('../gulp-revealjs')
+	;
+
+gulp.task('default', function() {
+	gulp.start('compile', 'images');
+});
+
+gulp.task('images', () => gulp.src('sources/img/**')
+	.pipe(gulp.dest('lections/img'))
+);
+
+gulp.task('compile', () => gulp.src('sources/*.pug')
+	.pipe(pug({}))
+	.pipe(reveal())
+	.pipe(gulp.dest('lections/'))
+);
